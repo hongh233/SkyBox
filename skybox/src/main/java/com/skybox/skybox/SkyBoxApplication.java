4ce606399871f0b4db951072c63187cc12b751a5
hongh931 <hn582183@dal.ca>
2024-09-11 19:39:49 -0300

    uploading backend framework

---
+++ skybox/src/main/java/com/skybox/skybox/SkyBoxApplication.java
@@ -0,0 +1,13 @@
+package com.skybox.skybox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyBoxApplication.class, args);
+	}
+
+}
---
+++ skybox/src/test/java/com/skybox/skybox/SkyBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skybox.skybox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
